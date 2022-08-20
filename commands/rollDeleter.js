2e22db706ad4f3e0b78d6a32b2f11f0be0283606
snygghugo <91157834+snygghugo@users.noreply.github.com>
2022-08-20 03:19:11 +0200

    Update rollDeleter.js

--- commands/rollDeleter.js
+++ commands/rollDeleter.js
@@ -24,6 +24,12 @@ module.exports = {
       await guild.roles.delete(role.id, "I ate it lol");
       await interaction.reply(`The roll ${rollToDelete} has been eaten, bub`);
     } catch (error) {
+      if (error.toString() === "DiscordAPIError: Missing Permissions") {
+        interaction.reply(
+          "I think the " + rollToDelete + "-roll is better left alone, bub"
+        );
+        return;
+      }
       console.log(error);
     }
   },
